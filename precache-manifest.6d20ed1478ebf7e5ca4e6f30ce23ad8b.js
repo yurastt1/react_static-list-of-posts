0ae2896a5bbef45a598b5bb06b3b1db1d2f45d37
Yurii <yurastt1@gmail.com>
2020-10-27 04:50:11 +0200

    Updates

---
+++ precache-manifest.6d20ed1478ebf7e5ca4e6f30ce23ad8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf5cc09082974a9c0a3d6f8cffb8442",
+    "url": "./index.html"
+  },
+  {
+    "revision": "745c54d8133db98b5d31",
+    "url": "./static/css/main.491acbb2.chunk.css"
+  },
+  {
+    "revision": "d36cf7d1584e4e69c9e6",
+    "url": "./static/js/2.0472d363.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0472d363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745c54d8133db98b5d31",
+    "url": "./static/js/main.11b77795.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
